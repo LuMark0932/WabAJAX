69c216c7b85a7be50c887034ae4c5e8481c32d2a
LuMark0932 <mk219961201@hotmail.com>
2023-01-09 09:04:30 +0800

    aa

--- WabAJAX/Controllers/ApiController.cs
+++ WabAJAX/Controllers/ApiController.cs
@@ -7,6 +7,7 @@ namespace WabAJAX.Controllers
     {
         public IActionResult Index(string name)
         {
+            System.Threading.Thread.Sleep(5000);
             if (string.IsNullOrEmpty(name))
                 name = "Ajax";
             return Content($"Hellow {name}","text/plain",Encoding.UTF8);
